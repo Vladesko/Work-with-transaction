ddd8508fc2f0c3f1f8ce39e16e3725cd2ce69fbd
Vladesko <borodich01@list.ru>
2024-06-25 17:07:02 +0300

    Added middleware without Exceptions

---
+++ WebApiBank/Commons/Middleware/CustomExceptionHandlerMiddleware.cs
@@ -0,0 +1,39 @@
+﻿using System.Net;
+using System.Text.Json;
+
+namespace WebApiBank.Commons.Middleware
+{
+    public class CustomExceptionHandlerMiddleware(RequestDelegate next)
+    {
+        private readonly RequestDelegate next = next;
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await next(context);
+            }
+            catch (Exception exception)
+            {
+
+                await HandleExceptionAsync(context, exception);
+            }
+        }
+
+        private async Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            var code = HttpStatusCode.InternalServerError;
+            string result = string.Empty;
+
+            //TODO: Add switch for exceptions 
+
+            context.Response.StatusCode = (int)code;
+            context.Response.ContentType = "application/json";
+
+            if (result == string.Empty)
+                result = JsonSerializer.Serialize(new { error = exception.Message });
+
+            return context.Response.WriteAsync(result);
+        }
+    }
+}
